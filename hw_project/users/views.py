b6723e2a244931862cbd1942dea08c31904ac5ab
Roman-K26 <romankuspys777@gmail.com>
2023-12-29 19:18:56 +0300

    password reset

--- hw_project/users/views.py
+++ hw_project/users/views.py
@@ -2,7 +2,14 @@ from django.shortcuts import render, redirect
 from django.views import View
 from django.contrib import messages
 
-from .forms import RegisterForm
+from django.contrib.auth.views import PasswordResetView
+from django.contrib.messages.views import SuccessMessageMixin
+from django.urls import reverse_lazy
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect
+
+from .forms import RegisterForm, LoginForm
 
 
 # Create your views here.
@@ -28,3 +35,17 @@ class RegisterView(View):
             messages.success(request, f"Вітаємо {username}. Ваш акаунт успішно створено")
             return redirect(to="users:signin")
         return render(request, self.template_name, {"form": form})
+
+class ResetPasswordView(SuccessMessageMixin, PasswordResetView):
+    template_name = 'users/password_reset.html'
+    email_template_name = 'users/password_reset_email.html'
+    html_email_template_name = 'users/password_reset_email.html'
+    success_url = reverse_lazy('users:password_reset_done')
+    success_message = "An email with instructions to reset your password has been sent to %(email)s."
+    subject_template_name = 'users/password_reset_subject.txt'
+
+@login_required
+def logoutuser(request):
+    logout(request)
+    return redirect(to='quotes:root')
+
