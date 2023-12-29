112664e7d4202bc61b242fee9e70c033c2054097
Roman-K26 <romankuspys777@gmail.com>
2023-12-29 19:54:33 +0300

    Commit 3

--- hw_project/users/urls.py
+++ hw_project/users/urls.py
@@ -9,7 +9,7 @@ app_name = 'users'
 urlpatterns = [
     path('signup/', views.RegisterView.as_view(), name='signup'),
     path('signin/',
-         LoginView.as_view(template_name='users/login.html', form_class=LoginForm, redirect_authenticated_user=True),
+         LoginView.as_view(template_name='users/login.html', form_class=LoginForm, redirect_authenticated_user=False),
          name='signin'),
     path('logout/', views.logoutuser, name='logout'),
     path('reset-password/', views.ResetPasswordView.as_view(), name='password_reset'),
